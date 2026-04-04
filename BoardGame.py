64ac29211f12bec598a466c7da132fcb09826605
rinku89mahato-tech <rinku89mahato@gmail.com>
2026-04-04 03:04:05 +0000

    Progress1_HW3

--- BoardGame.py
+++ BoardGame.py
@@ -1,6 +1,6 @@
 # Homework 3 - Board Game System
-# Name:
-# Date:
+# Name: Rinku Mahato
+# Date: 04/03/2026
 
 def loadGameData(filename):
     """Reads game data from a file and returns it as a list."""
@@ -20,7 +20,42 @@ def displayGame(data):
 
 def movePlayer(data):
     """Example function to simulate moving a player."""
-    print("\nMove player function not fully implemented.")
+    
+    player = input("Enteer player name: ")
+    steps = int(input("Enter steps to move: "))
+
+    index = -1
+    current_pos = 0 
+
+    #Find Player 
+    for i in range(len(data)):
+        parts = data[i].split(": ")
+        if not parts[0]. isdigit():
+                 continue
+        position,name = parts
+
+        if name == player:
+            index = i 
+            current_pos = int(position)
+            break
+
+    #if player not found
+    if index == -1:
+        print("player not found")
+        return 
+    
+    #move player 
+    new_pos = current_pos + steps
+    # check if somehting is already there
+    for item in data:
+        parts = item.split(": ")
+
+        if parts[0].isdigit() and int(parts[0]) == new_pos:
+            print(f"{player} landed on {parts[1]}!")
+
+    data[index] = f"{new_pos}: {player}"
+
+    print (f"{player}moved from {current_pos} to {new_pos}")
     # Students will modify this
 
 
@@ -28,12 +63,18 @@ def main():
     filename = "events.txt"   # Students can rename if needed
 
     gameData = loadGameData(filename)
-    displayGame(gameData)
 
-    # Example interaction
-    choice = input("\nMove player? (y/n): ")
-    if choice.lower() == "y":
-        movePlayer(gameData)
+    while True:
+        displayGame(gameData)
+
+    # Example innteraction
+        choice = input("\nMove player? (y/n): ").lower()
+        if choice == "y":
+            movePlayer(gameData)
+        else:
+            print("Goodbye!")
+            break
+
 
 
 if __name__ == "__main__":
